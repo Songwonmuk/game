7184186f5ce025bfad8943e42de9372afc218510
Songwonmuk <103928775+Songwonmuk@users.noreply.github.com>
2022-05-28 21:03:32 +0900

    Update MigrationService.java

--- src/net/scit/word/service/MigrationService.java
+++ src/net/scit/word/service/MigrationService.java
@@ -3,7 +3,6 @@ package net.scit.word.service;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 import net.scit.word.dao.WordDAO;
@@ -13,8 +12,8 @@ public class MigrationService {
 
 	WordDAO dao = new WordDAO();
 
-	public void migration() throws Exception{
-		
+	public void migration() throws Exception {
+
 		File file = new File("D:/user/Desktop/word.txt");
 		try (BufferedReader reader = new BufferedReader(
 				new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
@@ -33,5 +32,5 @@ public class MigrationService {
 			}
 			reader.close();
 		}
-		}
+	}
 }
